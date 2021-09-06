f597974e6a73aacbc54b6f7082c1eb1b26beb444
Mathias Jeppsson <mathias.jeppsson@catalina.com>
2021-09-07 02:06:29 +0900

    version

--- fs-store.rb
+++ fs-store.rb
@@ -3,7 +3,6 @@ class FsStore < Formula
   homepage "https://github.com/jeppsson/homebrew-fs-store"
   url "https://github.com/jeppsson/homebrew-fs-store/raw/master/libs/v0.0.1.zip"
   sha256 "33171bbe8d7960fb93260cfa733744fc8fd0a5def55f97925e67c94b1e99280"
-  version "master"
 
   def install
     inreplace "brew/fs-store", "##PREFIX##", "#{prefix}"
