0654d3b45f70645d6f9fadedc84e7e608883ccb4
Mathias Jeppsson <mathias.jeppsson@catalina.com>
2021-09-07 01:07:36 +0900

    Initial commit

---
+++ fs-store.rb
@@ -0,0 +1,13 @@
+require "formula"
+
+class fs-store < Formula
+  homepage "https://github.com/jeppsson/homebrew-fs-store"
+  url "https://github.com/jeppsson/homebrew-fs-store/raw/master/libs/fs-store-0.0.1.zip"
+  sha256 "f33171bbe8d7960fb93260cfa733744fc8fd0a5def55f97925e67c94b1e99280"
+
+  def install
+    inreplace "brew/fs-store", "##PREFIX##", "#{prefix}"
+    prefix.install "libs/fs-store.jar"
+    bin.install "brew/fs-store"
+  end
+end
